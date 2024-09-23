4cd940d3531160b1b54971eefe4e2b41df92d5e7
Salmi Mustapha <mustapha.salmi08000@gmail.com>
2024-09-24 01:05:26 +0200

    gestion stockage

--- back/controllers/OrderControler.js
+++ back/controllers/OrderControler.js
@@ -1,4 +1,6 @@
+const { ObjectId } = require("mongodb");
 const Order = require("../models/Order");
+const Product = require("../models/Product");
 
 module.exports.orders = async (req, res) => {
     let user_id = req.auth.userId;
@@ -21,10 +23,19 @@ module.exports.order = async (req, res) => {
 module.exports.create = async (req, res) => {
     const order = new Order({
         ...req.body
-    });
+    })
     order.save()
-        .then(() => res.status(201).json({ message: "Order created !" }))
+        .then(() => {
+            res.status(201).json({ message: "Order created !" })
+        })
         .catch((error) => res.status(400).json({ error }));
+
+        order.orderItems.forEach(async (item) => {
+            await Product.updateOne(
+                { _id: new ObjectId(item.product) },
+                { $inc: { stock: -item.quantity } }
+            );
+        });
 }
 
 module.exports.update = async (req, res) => {
@@ -37,6 +48,20 @@ module.exports.update = async (req, res) => {
 
 module.exports.delete = async (req, res) => {
     let user_id = req.auth.userId;
+    let order = await Order.findOne({ _id: req.params.id, user: user_id }).exec();
+
+    
+    if(!order){
+        return res.status(404).json({ message: "Order not found !" });
+    }
+    if (!order.isDelivered) {
+        order.orderItems.forEach(async (item) => {
+            await Product.updateOne(
+                { _id: new ObjectId(item.product) },
+                { $inc: { stock: item.quantity } }
+            );
+        });
+    }
     Order
         .deleteOne({ _id: req.params.id, user: user_id })
         .then(() => res.status(200).json({ message: "Order deleted !" }))
